70290cf04d67cb6140f61b2e2debcd08e21a07f7
lei <mia.wang90@gmail.com>
2022-11-11 18:13:52 +0900

    Add modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Learn JS" />
+      <Todo text="Learn Nextjs" />
+    </div>
+  );
 }
 
 export default App;
