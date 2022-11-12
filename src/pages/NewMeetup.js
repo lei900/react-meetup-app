726977d9d5106f666b494ba22f05b6bfc614d051
lei <mia.wang90@gmail.com>
2022-11-12 15:54:43 +0900

    Add useEffect() hook

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 export default function NewMeetupPage() {
+  const navigate = useNavigate();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-meetup-app-828b4-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +15,9 @@ export default function NewMeetupPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      navigate("/");
+    });
   }
 
   return (
